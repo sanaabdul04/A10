47440616273700541db9e3224160502ae71d92c9
sanaabdul04 <146795035+sanaabdul04@users.noreply.github.com>
2023-12-01 18:26:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Function to set a cookie
+function setCookie(cookieName, cookieValue, expirationDays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+// Function to get the value of a cookie
+function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieArray = decodedCookie.split(';');
+    for (var i = 0; i < cookieArray.length; i++) {
+        var cookie = cookieArray[i];
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) == 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return "";
+}
+
+// Display the cookie value on page load
+document.getElementById("cookieValue").innerText += getCookie("exampleCookie");
+
+// Set a cookie with the name "exampleCookie" and value "Hello, World!" with an expiration of 7 days
+setCookie("exampleCookie", "Hello, World!", 7);
